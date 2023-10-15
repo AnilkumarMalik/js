9f7e6fd727d05a2a181012450b41f320cc6ceaac
AnilkumarMalik <147518401+AnilkumarMalik@users.noreply.github.com>
2023-10-15 16:48:21 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) //we are using node.js, not browser
+
+console.log(3 
+    + 
+     3); //Code readability should be high
+
+console.log("Anil");
+
+
+let name="Anil"
+let age=23
+let isLoggedIn=false
+let state;
+
+// number => 2 to power 53 -1
+// bigint
+// string => "" 
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
