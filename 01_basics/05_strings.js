e661ff15a7c620a66bfc5a1a25213326025e4004
AnilkumarMalik <147518401+AnilkumarMalik@users.noreply.github.com>
2023-10-17 14:47:26 +0000

    new file

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,42 @@
+const name="anil"
+const age = 23
+
+//console.log(name + age + " value");
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+ const gameName = new String("anil-kumar");
+
+ //console.log(gameName[0]);
+ //console.log(gameName.__proto__);
+
+
+ //console.log(gameName.length);
+ //console.log(gameName.toUpperCase());
+
+ //console.log(gameName.charAt(2));
+ //console.log(gameName.indexOf('t'));
+
+
+ const newString = gameName.substring(0,4);
+ console.log(newString);
+
+
+ const anotherString = gameName.slice(-8,8)
+ console.log((anotherString));
+
+
+ const newStringOne = " anil   "
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+
+ const url = "https://anil.com/anil%20kumar"
+
+ console.log(url.replace('%20','-'));
+
+ console.log(url.includes('anil'));
+
+ console.log(gameName.split('-'));
+
+
+ 
