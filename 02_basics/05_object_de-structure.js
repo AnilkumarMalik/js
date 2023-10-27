b03423df7ef44750d8fe7b0571b80b9a87c3209c
AnilkumarMalik <147518401+AnilkumarMalik@users.noreply.github.com>
2023-10-27 13:11:05 +0000

    all

---
+++ 02_basics/05_object_de-structure.js
@@ -0,0 +1,25 @@
+
+const course = {
+    coursename: "JavaScript",
+    price: "999",
+    courseInstructor: "Anil"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "Anil",
+//     "coursename": "js",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
